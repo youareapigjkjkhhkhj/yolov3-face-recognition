0ef344e1ed1db3a40c767d681c43b8b957fb303f
Laugh <laugh12321@vip.qq.com>
2022-01-09 16:58:55 +0800

    Initial commit

--- setting/facenet_args.py
+++ setting/facenet_args.py
@@ -2,7 +2,7 @@
 from utils.utils import read_person_list, get_color_table
 from root_path import project_dir
 
-model_exp = project_dir + '/data/weights_facenet2/20180402-114759.pb'
+model_exp = project_dir + '/data/weights_facenet/20180402-114759.pb'
 base_face_dir = project_dir + '/data/base_face'  # 基图片文件夹
 base_face_csv = project_dir + '/data/base_face/vector.csv'  # 基图片转成128为nparray的npz文件
 svm_path = project_dir + '/data/weights_svm/svm.pkl'  # 分类器模型文件
